844d5dce3e7caad82ff8b8cb6b381857ae470d34
Manish Naga <manishnaga631@gmail.com>
2025-06-21 22:58:26 +0530

    FINAL

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # ✅ Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend domain
+    allow_origins=["*"],  # Allow frontend domain
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
